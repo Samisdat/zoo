f9733af7966a7663772e6251a00f9c0d638571dc
Bastian Pertz <bastian.pertz1@vodafone.com>
2020-03-08 23:07:44 +0100

    Import subscriber model

--- api/src/routes/subscribers.ts
+++ api/src/routes/subscribers.ts
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const Subscriber = require('../models/subscriber')
+import {Subscriber} from '../models/subscriber';
 
 // Getting all subscribers
 router.get('/', async (req, res) => {

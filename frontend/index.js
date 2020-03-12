0b5c173098ec4f80cac48d956dea139b46a2a94f
Bastian Pertz <bastian.pertz1@vodafone.com>
2020-03-12 23:37:23 +0100

    Edit footpaths

--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,9 @@
+
+let map;
+
 const initMap = () => {
 
-    const map = new google.maps.Map(document.getElementById('map'), {
+    map = new google.maps.Map(document.getElementById('map'), {
         zoom: 17,
         center: {lat: 51.23925648995369, lng: 7.11062378150634421},
         mapTypeId: 'terrain'
@@ -13,44 +16,190 @@ const initMap = () => {
     return map;
 }
 
-const addPolygone = (map, type, polygons) =>{
+const globalStore = {};
+let active;
 
-    for(const polygon of polygons){
+let mapPolygons = {
 
-        var buildingPoly = new google.maps.Polyline({
+};
+let mapMarkers = [];
+
+const setMapOnAll =  () => {
+
+    for(const mapPolygonId in mapPolygons){
+        mapPolygons[mapPolygonId].setMap(null);
+    }
+
+    for(const marker of mapMarkers){
+        marker.setMap(null);
+    }
+
+    mapPolygons = {};
+    mapMarkers = []
+
+}
+
+const plotPolygone = () =>{
+
+    for(const polygonId in globalStore){
+
+        const polygon = globalStore[polygonId];
+
+        let opacity = 0.2;
+
+        if(active === polygon.id){
+            opacity = 1;
+        }
+        console.log(opacity)
+
+        mapPolygons[polygon.id] = new google.maps.Polyline({
             path: polygon.coordinate,
             geodesic: true,
             strokeColor: '#FF0000',
-            strokeOpacity: 1.0,
-            strokeWeight: 2,
-            editable: true,
-            fillColor: '#FF0000',
-            fillOpacity: 0.35
+            strokeOpacity: opacity,
+            strokeWeight: 3,
+            /*editable: true,*/
         });
 
-        buildingPoly.setMap(map);
 
-        var marker = new google.maps.Marker({
+        const marker = new google.maps.Marker({
             position: polygon.coordinate[0],
             map: map,
-            title: polygon.name
+            title: polygon.name,
+            data:polygon.id
         });
+        marker.addListener('click', (marker) => {
+
+            active = polygon.id;
+            setMapOnAll(null)
+            plotPolygone();
+
+        });
+
+        mapMarkers.push(marker);
+
+        mapPolygons[polygon.id].setMap(map);
+
     }
 
 };
 
-const loadPolygon = (map, type) => {
+const setActive = (newAcive) => {
+    if(undefined !== active){
+        document.getElementById(active).classList.remove('active')
+    }
+
+    active = newAcive;
+
+    if(undefined !== active){
+        document.getElementById(active).classList.add('active')
+
+    }
+}
+
+const on = (elSelector, eventName, selector, fn) => {
+    var element = document.querySelector(elSelector);
+
+    element.addEventListener(eventName, function(event) {
+        var possibleTargets = element.querySelectorAll(selector);
+        var target = event.target;
+
+        for (var i = 0, l = possibleTargets.length; i < l; i++) {
+            var el = target;
+            var p = possibleTargets[i];
+
+            while(el && el !== element) {
+                if (el === p) {
+                    return fn.call(p, event);
+                }
+
+                el = el.parentNode;
+            }
+        }
+    });
+}
+
+const listPolygone = (type, polygons) =>{
+
+    const list = document.getElementById('list');
+
+    for(const polygon of polygons) {
+
+        const li = document.createElement('div');
+        li.innerHTML = '<button class="highlight">' + polygon.name + '</button> <button class="delete">Delete</button>';
+        li.setAttribute('id', polygon.id);
+        list.append(li)
+
+        if(undefined === active){
+            setActive(polygon.id);
+        }
+
+        globalStore[polygon.id] = polygon;
+
+    }
+
+
+    plotPolygone();
+
+    on('#list', 'click','.highlight',   (evt)=>{
+
+        const target = evt.target.parentElement;
+
+        const id = target.getAttribute('id');
+
+        setMapOnAll();
+        setActive(id);
+        plotPolygone();
+
+    })
+
+    on('#list', 'click','.delete',   (evt)=>{
+
+        const target = evt.target.parentElement;
+
+        const id = target.getAttribute('id');
+
+        deletePolygon(id)
+
+        delete globalStore[id];
+
+        setMapOnAll();
+        setActive(id);
+        plotPolygone();
+
+
+
+    })
+
+};
+
+const loadPolygon = (type) => {
 
     var xhr = new XMLHttpRequest();
 
-    xhr.open('GET', 'http://127.0.0.1:3000/' + type + '/gmap', false);
+    xhr.open('GET', 'http://127.0.0.1:3000/polygon/' + type, false);
     xhr.onload = function(responseText) {
-
-        console.log(responseText)
 
         const polygons = JSON.parse(this.responseText);
 
-        addPolygone(map, type, polygons);
+        listPolygone(type, polygons);
+
+    }
+    xhr.onerror = function() {
+        console.log('Error ' + this.status);
+    }
+    xhr.send();
+
+
+};
+
+const deletePolygon = (id) => {
+
+    var xhr = new XMLHttpRequest();
+
+    xhr.open('DELETE', 'http://127.0.0.1:3000/polygon/' + id, false);
+    xhr.onload = (responseText) => {
+
 
     }
     xhr.onerror = function() {
@@ -63,9 +212,9 @@ const loadPolygon = (map, type) => {
 
 const start = () => {
 
-    const map = initMap();
+    initMap();
 
-    loadPolygon(map, 'border');
+    loadPolygon('way');
 
 };
 

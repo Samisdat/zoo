c50e6dab88a45aea32caeb80023d84fc9941bf17
samisdat <bastian@pertz.eu>
2020-07-06 23:42:34 +0200

    Use material ui

--- react-frontend/pages/index.js
+++ react-frontend/pages/index.js
@@ -1,20 +1,95 @@
-import Link from 'next/link'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
-function Home() {
-    return (
-        <ul>
-        <li>
-        <Link href="/">
-        <a>Home</a>
-        </Link>
-        </li>
-        <li>
-        <Link href="/about">
-        <a>About Us</a>
-    </Link>
-    </li>
-    </ul>
-)
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+import AddIcon from '@material-ui/icons/Add';
+import Fab from '@material-ui/core/Fab';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Tooltip from '@material-ui/core/Tooltip';
+
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+
+
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.paper,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+    fab: {
+        margin: theme.spacing(2),
+    },
+    absolute: {
+        position: 'absolute',
+        bottom: theme.spacing(2),
+        right: theme.spacing(3),
+    },
+}));
+
+function ListItemLink(props) {
+    return <ListItem button component="a" {...props} />;
 }
 
-export default Home
+export default function ButtonAppBar() {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" className={classes.title}>
+                        News
+                    </Typography>
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
+
+
+            <Grid container spacing={3}>
+                <Grid item xs={4}>
+                    <List component="nav">
+                        <ListItemLink href="#simple-list">
+                            <ListItemText primary="Spam" />
+                        </ListItemLink>
+                    </List>
+                </Grid>
+                <Grid item xs={8}>
+                    <Paper className={classes.paper}>xs=8</Paper>
+                </Grid>
+            </Grid>
+
+        <Tooltip title="Delete">
+        <IconButton aria-label="delete">
+        <DeleteIcon />
+        </IconButton>
+        </Tooltip>
+
+        </div>
+);
+}

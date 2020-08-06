44567615adc397562a3eb0f800436d4ad8114e91
samisdat <bastian@pertz.eu>
2020-08-06 23:02:27 +0200

    typescript and copy over stuff

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,59 +1,49 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
-import {makeStyles, ThemeProvider} from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import theme from '../theme';
-
-import '../styles/styles.css'
+import { ThemeProvider } from '@material-ui/core/styles';
 import Grid from "@material-ui/core/Grid";
-import TopNavigation from "../components/TopNavigation";
+import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from '../src/theme';
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-    },
-    paper: {
-        padding: theme.spacing(2),
-        textAlign: 'center',
-        color: theme.palette.text.secondary,
-    },
-}));
+import TopNavigation from '../components/TopNavigation';
 
-export default function ZooBackend(props) {
-    const { Component, pageProps } = props;
+export default function MyApp(props) {
+  const { Component, pageProps } = props;
 
-    React.useEffect(() => {
-        // Remove the server-side injected CSS.
-        const jssStyles = document.querySelector('#jss-server-side');
-        if (jssStyles) {
-            jssStyles.parentElement.removeChild(jssStyles);
-        }
-    }, []);
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
 
-    const classes = useStyles();
-
-    return (
-        <React.Fragment>
-            <Head>
-                <title>My page</title>
-                <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-                <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" crossOrigin=""/>
-            </Head>
-            <ThemeProvider theme={theme}>
-                {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-                <CssBaseline />
-
-                <Component {...pageProps} />
-
-                s
-
-            </ThemeProvider>
-        </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      <Head>
+        <title>My page</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+          <Grid container>
+              <Grid item xs={12}>
+                  hei
+              </Grid>
+              <Grid item xs={12}>
+                Hei
+              </Grid>
+          </Grid>
+        <Component {...pageProps} />
+        <TopNavigation {...props}></TopNavigation>
+      </ThemeProvider>
+    </React.Fragment>
+  );
 }
 
-ZooBackend.propTypes = {
-    Component: PropTypes.elementType.isRequired,
-    pageProps: PropTypes.object.isRequired,
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
 };

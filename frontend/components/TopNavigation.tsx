af0d435f65cc0c8c3a27de8e5ac14c6d3e335474
samisdat <bastian@pertz.eu>
2020-08-08 00:03:23 +0200

    use menu icon

--- frontend/components/TopNavigation.tsx
+++ frontend/components/TopNavigation.tsx
@@ -6,7 +6,6 @@ import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import Fab from '@material-ui/core/Fab';
 import MenuIcon from '@material-ui/icons/Menu';
-import AddIcon from '@material-ui/icons/Add';
 import SearchIcon from '@material-ui/icons/Search';
 import MoreIcon from '@material-ui/icons/MoreVert';
 
@@ -52,7 +51,7 @@ export default function BottomAppBar() {
                     <MenuIcon />
                 </IconButton>
                 <Fab color="secondary" aria-label="add" className={classes.fabButton}>
-                    <AddIcon />
+                    <MenuIcon />
                 </Fab>
                 <div className={classes.grow} />
                 <IconButton color="inherit">

71c67b06e077804c9e3db746f7020c342b0aec75
samisdat <bastian@pertz.eu>
2020-08-08 00:04:16 +0200

    add leaftlet css

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -24,6 +24,7 @@ export default function MyApp(props) {
       <Head>
         <title>My page</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+          <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" crossOrigin=""/>
       </Head>
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}

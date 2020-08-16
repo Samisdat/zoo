e778f9debea76329196c1b5ae45f33a8882218d5
samisdat <bastian@pertz.eu>
2020-08-17 00:40:59 +0200

    refactor

--- frontend/components/Map.tsx
+++ frontend/components/Map.tsx
@@ -3,14 +3,15 @@ import React, { useEffect } from 'react';
 import * as d3 from 'd3';
 
 import {usePersistedState} from "../hooks/persisted-state";
+import {createReadStream} from "fs";
 
 export default function ZooMap() {
 
     const mapId = 'zoo-map';
 
     const [marker, setMarker] = usePersistedState('marker', {
-        lat: 51.23943863918227,
-        lng: 7.111485600471497
+        lat: 51.238741,
+        lng: 7.107757
     });
 
     const [center, setCenter] = usePersistedState('center', {
@@ -26,39 +27,30 @@ export default function ZooMap() {
     }
 
     getPosition({})
-        .then((position) => {
-            console.log(position);
-        })
-        .catch((err) => {
-            console.error(err.message);
-        });
+    .then((position) => {
+        console.log(position);
+    })
+    .catch((err) => {
+        console.error(err.message);
+    });
 
-    useEffect(() => {
-        const width = 800;
-        const height = 700;
+    const createMap = (jsons) => {
 
-        const bound = {
-            south: 51.236776961813064,
-            west: 7.105611562728882,
-            north: 51.24177020918754,
-            east: 7.115809321403503
-        };
+        const borders = jsons[0];
+        const ways = jsons[1];
+        const water = jsons[2];
+        const buildings = jsons[3];
 
-        /*
-        const centerLat = (bound.south + bound.north) / 2;
-        const centerLng = (bound.west + bound.east) / 2;
-         */
-
-        const centerLat = center.lat;
-        const centerLng = center.lng
+        const width = 600;
+        const height = 500;
 
         const projection = d3.geoMercator()
-            .scale(5000000)
+            .scale(3000000)
             .rotate([0, 0, 0])
-            .center([centerLng, centerLat])
+            .center([center.lng, center.lat])
             .translate([width / 2, height / 2]);
 
-        //projection.angle(90)
+        // projection.angle(181.5)
 
         const geoPath = d3.geoPath()
             .projection(projection);
@@ -68,11 +60,13 @@ export default function ZooMap() {
         var svg = d3.select(`#${mapId}`).append("svg")
             .attr("width", width + 'px')
             .attr("height", height + 'px')
+            .attr("style", 'background:red')
         ;
 
         const addGeoJson = (data, fill) => {
 
             let g = svg.append("g");
+            console.log(data)
 
             g.selectAll("path")
                 .data(data)
@@ -89,22 +83,6 @@ export default function ZooMap() {
             let lng = marker.lng;
             let lat = marker.lat;
 
-            lng = 7.107757;
-            lat = 51.238741;
-            /*
-            let mysteriosLng = 7.110321521759034 - 7.108728
-            let mysteriosLat = 51.240815597945485 - 51.24077
-
-            console.log(mysteriosLng, mysteriosLat);
-
-            console.log(lng, lat);
-
-
-            lng = lng - mysteriosLng
-            lat = lat - mysteriosLat
-
-            console.log(lng, lat);
-            */
             var currentPositionCollection = {
                 "type": "FeatureCollection",
                 "features": [
@@ -117,21 +95,6 @@ export default function ZooMap() {
 
                 ]
             };
-
-            /*
-            var currentPositionCollection = {
-                "type": "FeatureCollection",
-                "features": [
-                    {
-                        "type": "Feature",
-                        "geometry": { "type": "Point", "coordinates": [
-                                7.108265,
-                                51.239674 ] }
-                    },
-
-                ]
-            };
-            */
 
             var rodents = svg.append( "g" );
 
@@ -147,6 +110,28 @@ export default function ZooMap() {
 
         };
 
+        addGeoJson(borders.features, '#B6E2B6')
+        addGeoJson(water.features, '#AADAFF')
+        addGeoJson(ways.features, '#fff')
+        addGeoJson(buildings.features, '#C7C7B4')
+
+        addCurrentPosition();
+
+        var g = svg.selectAll('g');
+        var zoom = d3.zoom()
+            .scaleExtent([0.5, 8])
+            .on('zoom', function() {
+                g.selectAll('path')
+                    .attr('transform', d3.event.transform);
+
+            });
+
+        svg.call(zoom);
+
+    };
+
+    useEffect(() => {
+
         Promise.all([
             d3.json('/api/geojson/borders'),
             d3.json('/api/geojson/ways'),
@@ -154,24 +139,7 @@ export default function ZooMap() {
             d3.json('/api/geojson/buildings')
         ]).then(function(files) {
 
-            addGeoJson(files[0].features, '#B6E2B6')
-            addGeoJson(files[2].features, '#AADAFF')
-            addGeoJson(files[1].features, '#fff')
-            addGeoJson(files[3].features, '#C7C7B4')
-
-            addCurrentPosition();
-
-
-            var g = svg.selectAll('g');
-            var zoom = d3.zoom()
-                .scaleExtent([0.5, 8])
-                .on('zoom', function() {
-                    g.selectAll('path')
-                        .attr('transform', d3.event.transform);
-
-            });
-
-            svg.call(zoom);
+            createMap(files);
 
         }).catch(function(err) {
             // handle error here
@@ -191,6 +159,7 @@ export default function ZooMap() {
                     left:0,
                     width: '800px',
                     height: '700px',
+                    opacity:0,
                     background: 'url(/luftaufnahme.png)',
                     backgroundSize: 'cover',
                 }}></div>
@@ -198,7 +167,7 @@ export default function ZooMap() {
                     position:'absolute',
                     top:0,
                     left:0,
-                    opacity:0.7,
+                    opacity:1,
                     width: '800px',
                     height: '700px'
                 }}

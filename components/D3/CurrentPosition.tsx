853e24dcbc0ea03f10d07d5375a2636f17b4da69
Bastian Pertz <bastian.pertz1@vodafone.com>
2020-09-18 20:43:07 +0200

    mark current position with an black circle

--- components/D3/CurrentPosition.tsx
+++ components/D3/CurrentPosition.tsx
@@ -1,8 +1,14 @@
-import {useEffect, useState} from 'react';
+import React, {useEffect, useState} from 'react';
+import * as d3 from 'd3';
+
+import {getCurrentPositionGeoJson} from 'helper/getCurrentPosition';
+
 
 export default function CurrentPosition(props) {
 
-    const anId = 'anId';
+    const svgId = 'main-svg';
+
+    const positionId = 'main-position';
 
     useEffect(() => {
 
@@ -13,19 +19,34 @@ export default function CurrentPosition(props) {
             return;
         }
 
-        console.log(props)
-        console.log('really start usefx current pos');
-        console.log(props)
-        document.getElementById(anId).innerHTML = JSON.stringify(props.d3PropertiesState);
+        var mapSvg = d3.select(`#${svgId}`)
+
+        var positionGroup = mapSvg.select(`#${positionId}`);
+        const currentPosition = getCurrentPositionGeoJson('initial', props.d3PropertiesState.marker.lat, props.d3PropertiesState.marker.lng);
+
+        positionGroup.selectAll("circle")
+            .data(currentPosition)
+            .join("circle")
+
+            .attr("transform", function(d) { return "translate(" + props.d3PropertiesState.geoPath.centroid(d) + ")"; })
+            .attr("title", (d)=>{
+                return d.properties.slug;
+            })
+            .attr("fill", (d, i)=>{
+                return d.properties.fill;
+            })
+            .attr("stroke", (d)=>{
+                return d.properties.stroke;
+            })
+            .attr("d", props.d3PropertiesState.geoPath)
+            .attr("r", 5);
+
 
     });
 
     const text = 'Kind'
     return (
-        <div>
-            <div>{text}</div>
-            <div id={anId}></div>
-        </div>
+        <g id={positionId}></g>
     );
 
 }
--- components/D3/Map.tsx
+++ components/D3/Map.tsx
@@ -7,7 +7,8 @@ import CurrentPosition from 'components/D3/CurrentPosition';
 
 export default function Parent(props) {
 
-    const text = 'Map'
+    const svgId = 'main-svg';
+    const mapId = 'main-map';
 
     const [d3PropertiesState, setD3PropertiesState] = usePersistedState('d3',undefined);
 
@@ -22,10 +23,59 @@ export default function Parent(props) {
 
         const geoPath =  d3.geoPath().projection(projection);
 
+        var center = d3.geoCentroid(props.border);
+
+        projection
+            .scale(3000000)
+            .center(center)
+
+        var mapSvg = d3.select(`#${svgId}`)
+            .attr("width", width + 'px')
+            .attr("height", height + 'px')
+            .attr("style", 'background:blue')
+        ;
+
+        const mapGroup = mapSvg.select(`#${mapId}`);
+
+        function onClick() {
+
+            const position = projection.invert(d3.mouse(this))
+
+            console.log(position)
+
+        };
+
+        mapSvg.on("click", onClick);
+
+        var zooming = d3.zoom()
+            .scaleExtent([0.5, 8])
+            .on('zoom', () => {
+
+                mapGroup
+                    .attr('transform', d3.event.transform);
+
+            })
+            .on('end', () => {
+                console.log(d3.event.transform);
+                //setTransform(d3.event.transform);
+
+            });
+
+        mapSvg.call(zooming);
+
+        const marker = {
+            lat: 51.238741,
+            lng: 7.107757,
+            isWithin: true,
+            text: 'Map Marker Text'
+        };
+
+
         const d3Properties = {
-            width:width,
-            height:height,
-            geoPath:geoPath
+            width: width,
+            height: height,
+            geoPath: geoPath,
+            marker: marker
         };
 
         setD3PropertiesState(d3Properties);
@@ -43,9 +93,17 @@ export default function Parent(props) {
 
 
     return (
-        <div id="parent">
-            <div>{text}</div>
-            <CurrentPosition d3PropertiesState={d3PropertiesState}></CurrentPosition>
+        <div>
+            <svg id={svgId} style={{
+                width: '100%',
+                height: '100%',
+                background: 'red'
+            }}
+            >
+                <CurrentPosition d3PropertiesState={d3PropertiesState}></CurrentPosition>
+            </svg>
+
+
         </div>
     );
 

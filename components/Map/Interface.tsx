fbcd10b9b4cd279ee82dc4fe60b7eeda933f36ac
samisdat <bastian@pertz.eu>
2020-12-22 18:04:45 +0100

    Move interfaces into seperate path

---
+++ components/Map/Interface.tsx
@@ -0,0 +1,26 @@
+import {GeoPath} from "d3";
+
+export interface MarkerInterface {
+    lat: number;
+    lng: number;
+    isWithin: boolean;
+    isGPS: boolean
+    text: string;
+}
+
+export interface MapTransformInterface {
+    x: number;
+    y: number;
+    k: number;
+}
+
+export interface MapStateInterface {
+    width: number;
+    height: number;
+    dimensionUnit: string;
+    color: string;
+    focus: string;
+    marker: MarkerInterface;
+    pathGenerator: GeoPath,
+    transform: MapTransformInterface;
+}

6efb2dd1ef4684cbd29fdaf4ae437e48e2c8511c
samisdat <bastian@pertz.eu>
2020-12-29 00:08:44 +0100

    Move setFocus up to app

--- components/Navigation/Interfaces.ts
+++ components/Navigation/Interfaces.ts
@@ -1,8 +1,15 @@
+import {Feature, Polygon} from "geojson";
+
 export type NavigationMainItems = 'map' | 'species' | 'search' | 'menu';
+
+export type MapFocus = 'none';
 
 export interface NavigationInterface{
     activeMainItem:NavigationMainItems;
     openSideMenu: boolean;
     openTeaser: boolean;
     openSearch: boolean;
+    focus: MapFocus | Feature<Polygon>;
+    foobar:string;
+
 }

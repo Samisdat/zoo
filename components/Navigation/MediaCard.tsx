65c3e303a6bdce000c44dce14a69daa9aaacf36f
samisdat <bastian@pertz.eu>
2020-12-30 00:48:06 +0100

    typo

--- components/Navigation/MediaCard.tsx
+++ components/Navigation/MediaCard.tsx
@@ -47,7 +47,7 @@ export default function MediaControlCard(props) {
         >
             <CardMedia
                 className={classes.cover}
-                image="/images/elephant.jpg"
+                image="/images/elefant.jpg"
                 title="Live from space album cover"
             />
             <div className={classes.details}>

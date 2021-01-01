2c03d5e33368c049441e3440cff8463e04788b88
samisdat <bastian@pertz.eu>
2021-01-01 22:49:24 +0100

    use markdown for animals

--- pages/animal/[slug].tsx
+++ pages/animal/[slug].tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import { useRouter } from 'next/router'
 import {getAnimals, getAnimal, Animal} from "pages/api/animals";
+import renderToString from 'next-mdx-remote/render-to-string'
+import hydrate from 'next-mdx-remote/hydrate'
 
 export default function Tiere(props) {
 
     const router = useRouter()
     const { slug } = router.query
 
+    const content = hydrate(props.source, {  })
+
     return (
-        <React.Fragment>{slug} {props.animal.name}</React.Fragment>
+        <React.Fragment>
+            {slug} {props.animal.name}
+            <div>{content}</div>
+        </React.Fragment>
     );
 }
 
@@ -16,9 +23,13 @@ export async function getStaticProps(context) {
 
     const animal = await getAnimal(context.params.slug);
 
+    // MDX text - can be from a local file, database, anywhere
+    const source = 'Some **mdx** text, with a component'
+    const mdxSource = await renderToString(source, {})
     return {
         props: {
-            animal:animal
+            animal:animal,
+            source: mdxSource
         }
     }
 }

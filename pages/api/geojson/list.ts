c9315ed3a83c3024b319e4e95da57996c52aca35
samisdat <bastian@pertz.eu>
2021-03-14 22:45:58 +0100

    use get from data-repos geojson

--- pages/api/geojson/list.ts
+++ pages/api/geojson/list.ts
@@ -1,57 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
-import {Feature, FeatureCollection, Point, Polygon} from "geojson";
+import {FeatureCollection, Point, Polygon} from "geojson";
 import {get} from "../../../data-repos/geojson";
-
-const getBorder = async ():Promise<FeatureCollection> => {
-
-    const border = await get('border') as FeatureCollection<Polygon>;
-
-    return border;
-
-}
-
-const getWays = async ():Promise<FeatureCollection> => {
-
-    const ways = await get('way') as FeatureCollection<Polygon>;
-
-    // @TODO does sorting make sense on this side?
-    ways.features = ways.features.sort( (a:Feature<Polygon>, b:Feature<Polygon>)=>{
-        return a.properties.name.localeCompare(b.properties.name);
-    });
-
-    return ways;
-
-}
-
-const getFacilityBoxes = async ():Promise<FeatureCollection> => {
-
-    const facilityBoxes = await get('facility-box') as FeatureCollection<Polygon>;
-
-    // @TODO does sorting make sense on this side?
-    facilityBoxes.features = facilityBoxes.features.sort( (a:Feature<Polygon>, b:Feature<Polygon>)=>{
-        return a.properties.name.localeCompare(b.properties.name);
-    });
-
-    return facilityBoxes;
-
-}
-
-const getFacilityCircles = async ():Promise<FeatureCollection> => {
-
-    const facilityCircles = await get('facility-circle') as FeatureCollection<Point>;
-
-    return facilityCircles;
-
-}
-
-const getBoundingBox = async ():Promise<FeatureCollection> => {
-
-    const boundingBox = await get('bounding-box') as FeatureCollection<Polygon>;
-
-    return boundingBox;
-
-}
 
 const emptyGeoJson:FeatureCollection = {
     "type": "FeatureCollection",
@@ -64,19 +14,26 @@ export const getFullGeoJson = async (): Promise<FeatureCollection> => {
         ...emptyGeoJson
     };
 
-    const facilityBoxes = await getFacilityBoxes();
+    let facilityBoxes = await get('facility-box') as FeatureCollection<Polygon>;
+
+    facilityBoxes.features = facilityBoxes.features.filter((feature)=>{
+
+        return (false !== feature.properties?.published);
+
+    });
+
     geoJson.features = geoJson.features.concat(facilityBoxes.features);
 
-    const facilityCircles = await getFacilityCircles();
+    const facilityCircles = await get('facility-circle') as FeatureCollection<Point>;
     geoJson.features = geoJson.features.concat(facilityCircles.features);
 
-    const boundingBox = await getBoundingBox();
+    const boundingBox = await get('bounding-box') as FeatureCollection<Polygon>;
     geoJson.features = geoJson.features.concat(boundingBox.features);
 
-    const ways = await getWays();
+    const ways = await get('way') as FeatureCollection<Polygon>;
     geoJson.features = geoJson.features.concat(ways.features);
 
-    const border = await getBorder();
+    const border = await get('border') as FeatureCollection<Polygon>;
     geoJson.features = geoJson.features.concat(border.features);
 
     return geoJson;

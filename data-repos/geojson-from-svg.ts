5bc5a4c0536df465f289dec376e16d4b008f50ba
samisdat <bastian@pertz.eu>
2021-03-16 22:58:55 +0100

    get svg and test

--- data-repos/geojson-from-svg.ts
+++ data-repos/geojson-from-svg.ts
@@ -1,3 +1,27 @@
-export const geojsonFromSvg = ()=>{
+import path from "path";
+import fs from "fs";
+import {getRootDataDir} from "./data-helper";
+
+export const getSvgPath = () => {
+
+    return path.resolve(
+        getRootDataDir(),
+        'svg',
+        'combined.svg'
+    );
+
+}
+
+export const getSvg = () => {
+
+    const combinedSvg = fs.readFileSync(
+        getSvgPath(),
+        {encoding:'utf8'}
+    );
+    return combinedSvg;
+
+}
+
+export const geojsonFromSvg = (slug:string)=>{
     console.log('geojson-from-svg');
 }

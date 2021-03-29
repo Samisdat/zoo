62c0689d8ee8d0117a3e64d203079f0f97ed8eb1
samisdat <bastian@pertz.eu>
2021-03-30 00:03:29 +0200

    Install strapi

---
+++ starpi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '930efefaf9e29316fb535725aaae1144'),
+    },
+  },
+});
---
+++ starpi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '141194a1-12e1-475f-9bea-fa21189ce439'
+};

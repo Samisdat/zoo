8f763fc40c80cdf469553f8cbaeee8a17311b342
samisdat <bastian@pertz.eu>
2021-05-30 16:56:11 +0200

    Mobile Navigation

--- frontend/components/Navigation/Navigation.tsx
+++ frontend/components/Navigation/Navigation.tsx
@@ -2,13 +2,19 @@ import React from 'react';
 import {useViewport} from "../viewport/useViewport";
 import NavigationMobile from "./NavigationMobile";
 import {useRouter} from "next/router";
-import NavigationMedium from "./NavigationMedium";
+import {NavigationMedium} from "./NavigationMedium";
 
 export const Navigation = (props) => {
 
     const { width, height, breakpoint } = useViewport();
 
     const router = useRouter();
+
+    return <NavigationMedium
+        router={router}
+        {...props}
+    />
+
 
     if('extra-small' === breakpoint){
         return <NavigationMobile
--- frontend/components/Navigation/NavigationMedium.tsx
+++ frontend/components/Navigation/NavigationMedium.tsx
@@ -1,18 +1,42 @@
 import React from 'react';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import useScrollTrigger from '@material-ui/core/useScrollTrigger';
-import Slide from '@material-ui/core/Slide';
-import {createStyles, makeStyles, Theme} from "@material-ui/core/styles";
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import Slide from "@material-ui/core/Slide";
+import List from "@material-ui/core/List";
 import {ListItem, ListItemText} from "@material-ui/core";
 import Container from "@material-ui/core/Container";
-import IconButton from "@material-ui/core/IconButton";
-import {Home} from "@material-ui/icons";
-import List from "@material-ui/core/List";
-import CloseIcon from "@material-ui/icons/Close";
-import MenuIcon from "@material-ui/icons/Menu";
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            flexGrow: 1,
+            flexDirection: 'inherit',
+        },
+        toolbarPadding:{
+            ...theme.mixins.toolbar,
+        },
+        menuButton: {
+            marginRight: theme.spacing(1),
+        },
+        title: {
+            flexGrow: 1,
+        },
+        navDisplayFlex: {
+            display: `flex`,
+            justifyContent: `space-between`,
+        },
+        linkText: {
+            textDecoration: `none`,
+            color: `white`
+        },
+    }),
+);
 
 interface Props {
     /**
@@ -37,82 +61,45 @@ function HideOnScroll(props: Props) {
     );
 }
 
-const useStyles = makeStyles((theme: Theme) => {
-
-    return createStyles({
-        root: {
-            flexGrow: 1,
-        },
-        toolbarPadding:{
-            ...theme.mixins.toolbar,
-        },
-        navbarDisplayFlex: {
-            display: `flex`,
-            justifyContent: `space-between`
-        },
-        navDisplayFlex: {
-            display: `flex`,
-            justifyContent: `space-between`
-        },
-        linkText: {
-            textDecoration: `none`,
-            textTransform: `uppercase`,
-            color: `white`
-        },
-        title: {
-            display: `flex`,
-            justifyContent: `space-between`,
-            marginLeft: theme.spacing(2),
-            flex: 1,
-            flexGrow: 1,
-            verticalAlign: 'middle',
-        },
-    })
-});
-
 const navLinks = [
-    { title: `about us`, path: `/about-us` },
+    { title: `Der grüne Zoo`, path: `/about-us` },
     { title: `product`, path: `/product` },
     { title: `blog`, path: `/blog` },
     { title: `contact`, path: `/contact` },
     { title: `faq`, path: `/faq` }
 ];
 
-export default function NavigationMedium(props: Props) {
+export const NavigationMedium = (props) => {
 
     const classes = useStyles();
 
     return (
-        <React.Fragment>
+        <div className={classes.root}>
             <div className={classes.toolbarPadding}></div>
             <HideOnScroll {...props}>
-                <AppBar>
-                    <Toolbar>
-                        <Container className={classes.navbarDisplayFlex}>
-                            <IconButton edge="start" color="inherit">
-                                <MenuIcon />
-                            </IconButton>
-                            <Typography variant="h6" className={classes.title}>
-                                Der grüne Zoo
-                            </Typography>
-
-                            <List
-                                component="nav"
-                                className={classes.navDisplayFlex}
-                            >
-                                {navLinks.map(({ title, path }) => (
-                                    <a href={path} key={title} className={classes.linkText}>
-                                        <ListItem button>
-                                            <ListItemText primary={title} />
-                                        </ListItem>
-                                    </a>
-                                ))}
-                            </List>
-                        </Container>
-
-                    </Toolbar>
-                </AppBar>
+            <AppBar>
+                <Toolbar>
+                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" className={classes.title}>
+                        Der grüne Zoo
+                    </Typography>
+                    <List
+                        component="nav"
+                        className={classes.navDisplayFlex}
+                    >
+                        {navLinks.map(({ title, path }) => (
+                            <a href={path} key={title} className={classes.linkText}>
+                                <ListItem button>
+                                    <ListItemText primary={title} />
+                                </ListItem>
+                            </a>
+                        ))}
+                    </List>
+                </Toolbar>
+            </AppBar>
             </HideOnScroll>
-        </React.Fragment>
+        </div>
     );
 }

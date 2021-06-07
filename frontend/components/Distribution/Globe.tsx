af106ed970a3fe8210eb07eede3230c4b7ccb486
samisdat <bastian@pertz.eu>
2021-06-07 22:15:59 +0200

    use const and let instead of var

--- frontend/components/Distribution/Globe.tsx
+++ frontend/components/Distribution/Globe.tsx
@@ -2,18 +2,16 @@ import React, {useEffect, useRef} from 'react';
 
 import * as d3 from 'd3';
 
-
-
 //interpolator from http://bl.ocks.org/jasondavies/4183701
 const d3_geo_greatArcInterpolator = function() {
-    var d3_radians = Math.PI / 180;
-    var x0, y0, cy0, sy0, kx0, ky0,
+    const d3_radians = Math.PI / 180;
+    let x0, y0, cy0, sy0, kx0, ky0,
         x1, y1, cy1, sy1, kx1, ky1,
         d,
         k;
 
     function interpolate(t) {
-        var B = Math.sin(t *= d) * k,
+        let B = Math.sin(t *= d) * k,
             A = Math.sin(d - t) * k,
             x = A * kx0 + B * kx1,
             y = A * ky0 + B * ky1,
@@ -30,7 +28,7 @@ const d3_geo_greatArcInterpolator = function() {
     };
 
     interpolate.source = function(_) {
-        var cx0 = Math.cos(x0 = _[0] * d3_radians),
+        let cx0 = Math.cos(x0 = _[0] * d3_radians),
             sx0 = Math.sin(x0);
         cy0 = Math.cos(y0 = _[1] * d3_radians);
         sy0 = Math.sin(y0);
@@ -41,7 +39,7 @@ const d3_geo_greatArcInterpolator = function() {
     };
 
     interpolate.target = function(_) {
-        var cx1 = Math.cos(x1 = _[0] * d3_radians),
+        let cx1 = Math.cos(x1 = _[0] * d3_radians),
             sx1 = Math.sin(x1);
         cy1 = Math.cos(y1 = _[1] * d3_radians);
         sy1 = Math.sin(y1);
@@ -56,7 +54,7 @@ const d3_geo_greatArcInterpolator = function() {
 
 //make the plane always align with the direction of travel
 const calcAngle = (originalRotate, newRotate) => {
-    var deltaX = newRotate[0] - originalRotate[0],
+    const deltaX = newRotate[0] - originalRotate[0],
         deltaY = newRotate[1] - originalRotate[1]
 
     return Math.atan2(deltaY, deltaX);

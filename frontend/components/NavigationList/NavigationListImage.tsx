6e10072ddbe46cd0381310a27b198c2ad25f6015
samisdat <bastian@pertz.eu>
2021-06-07 23:20:36 +0200

    larger avatar in navi list

--- frontend/components/NavigationList/NavigationListImage.tsx
+++ frontend/components/NavigationList/NavigationListImage.tsx
@@ -2,6 +2,20 @@ import React from "react";
 import {Avatar, ListItemAvatar, ListItemIcon} from "@material-ui/core";
 import {NavigationListIcon} from "./NavigationListIcon";
 import {NavigationListItemInterface} from "./NavigationListInterfaces";
+import {createStyles, makeStyles, Theme} from "@material-ui/core/styles";
+
+export const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        large: {
+            height: theme.spacing(7),
+            minWidth: theme.spacing(9),
+        },
+        largeAvatar: {
+            width: theme.spacing(7),
+            height: theme.spacing(7),
+        },
+    })
+);
 
 export const NavigationListImage = (props: NavigationListItemInterface) => {
 
@@ -21,10 +35,15 @@ export const NavigationListImage = (props: NavigationListItemInterface) => {
 
     }
 
+    const classes = useStyles();
+
     if (undefined !== props.image) {
 
-        return (<ListItemAvatar>
+        return (<ListItemAvatar
+            className={classes.large}
+        >
             <Avatar
+                className={classes.largeAvatar}
                 alt={props.text}
                 src={props.image}
             />

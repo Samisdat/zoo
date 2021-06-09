e7b5374cbb6c8f234efe3369e67ee6ef4b0b6f11
samisdat <bastian@pertz.eu>
2021-06-09 02:31:40 +0200

    Remove not longer used interfaces

--- frontend/components/Navigation/Interfaces.ts
+++
@@ -1,9 +0,0 @@
-import {Feature, Polygon} from "geojson";
-
-export type NavigationMainItems = 'map' | 'species' | 'search' | 'menu';
-
-export interface NavigationInterface{
-    activeMainItem:NavigationMainItems;
-    openSideMenu: boolean;
-    openTeaser: boolean;
-}

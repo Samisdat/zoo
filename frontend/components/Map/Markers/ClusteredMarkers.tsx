be913113fd33bb0d81e1ab0a47ce92cbe8dfa3cd
samisdat <bastian@pertz.eu>
2022-01-06 17:26:37 +0100

    indicate when clustered

--- frontend/components/Map/Markers/ClusteredMarkers.tsx
+++ frontend/components/Map/Markers/ClusteredMarkers.tsx
@@ -64,10 +64,10 @@ export const ClusteredMarkers = (props:ClusteredMarkersProperties) => {
             })
             .attr('stroke', (d, i)=>{
 
-                return 'red';
+                return '#000';
             })
             .attr('stroke-width', (d, i)=>{
-                return 2;
+                return 1;
             })
             .attr('vector-effect', (d, i)=>{
                 return 'non-scaling-stroke'
@@ -77,35 +77,45 @@ export const ClusteredMarkers = (props:ClusteredMarkersProperties) => {
 
                 return ("url(#"+d.contains[0].id + "-icon)");
 
-                return 'blue';
             })
             .attr('r', props.radius)
             .on("click", handleClick);
         ;
 
-        groupSelection.selectAll('text')
-            .data(props.clusters)
-            .join('text')
-            .attr('x', function(d) {
-                return path.centroid( (d.contains[0] as Feature))[0];
+        groupSelection.selectAll('path')
+            .data(
+                props.clusters.filter(
+                    (cluster)=>{
+                        return (1 < cluster.contains.length)
+                    }
+                ))
+
+            .join('path')
+            .attr('width', function(d) {
+                return props.radius;
             })
-            .attr('y', function(d) {
-                return path.centroid( (d.contains[0] as Feature))[1];
+            .attr('height', function(d) {
+                return props.radius + 'px';
             })
-            .text((d) => {
-                //return d.contains[0].id;
-                return d.contains.length
+            .attr("transform", (d) => {
+
+                const scale = props.radius / 512;
+                const x = path.centroid( (d.contains[0] as Feature))[0] + props.radius / 4;
+                const y = path.centroid( (d.contains[0] as Feature))[1] + props.radius / 4;
+
+
+                const transform = "translate(" + x + "," + y + ") scale(" + scale +  ")";
+
+                return transform;
+
             })
             .attr('fill', (d, i)=>{
 
-                //return ("url(#"+d.contains[0].id + "-icon)");
+                return '#fff';
 
-                return 'white';
             })
-            .attr('width', props.radius/1.5)
-            .attr('height', props.radius/1.5)
-            .attr('font-size', props.radius/1.5)
-
+            .attr('d', 'M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm144 276c0 6.6-5.4 12-12 12h-92v92c0 6.6-5.4 12-12 12h-56c-6.6 0-12-5.4-12-12v-92h-92c-6.6 0-12-5.4-12-12v-56c0-6.6 5.4-12 12-12h92v-92c0-6.6 5.4-12 12-12h56c6.6 0 12 5.4 12 12v92h92c6.6 0 12 5.4 12 12v56z')
+        ;
 
     });
 

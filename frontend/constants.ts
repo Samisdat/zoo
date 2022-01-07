a46b5e3bdc4def910e44e50371191e6143ad52a8
samisdat <bastian@pertz.eu>
2022-01-08 00:05:10 +0100

    const for protocol, domain and qr path

--- frontend/constants.ts
+++ frontend/constants.ts
@@ -1,5 +1,11 @@
+export const protocol = 'http://'
+
+export const domain = '192.168.178.21:8080'
+
 export const facilityUrlPart = 'anlagen';
 
 export const animalUrlPart = 'tiere';
 
 export const blogUrlPart = 'blog';
+
+export const qrCodeUrlPart = 'qr-code';

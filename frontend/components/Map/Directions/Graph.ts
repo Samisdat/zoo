1b829ad7edafa05a0ef17da6b6ad3c4bfa98d2e1
samisdat <bastian@pertz.eu>
2022-01-19 22:44:29 +0100

    Create a Graph from paths

---
+++ frontend/components/Map/Directions/Graph.ts
@@ -0,0 +1,155 @@
+interface Node {
+    id: number;
+    x: number;
+    y: number;
+}
+
+interface Edge {
+    start: number;
+    end: number;
+    length: number;
+}
+
+interface NodesMapping{
+    [key:string]: number
+}
+
+
+export class Graph{
+
+    private nextNodeId = 0;
+
+    private edges:Edge[] = [];
+    private nodes:Node[] = [];
+
+    private nodesMapping: NodesMapping = {}
+
+    private constructor(segments:d3.Selection<d3.BaseType, unknown, any, unknown>) {
+
+        console.log('constructor')
+
+        segments.each((d,i)=> {
+            const path = segments.nodes()[i];
+            this.addEdge(path);
+        })
+
+    }
+
+    public addEdge(path:d3.BaseType){
+
+        const pathElement = path as SVGGeometryElement;
+
+        const length = pathElement.getTotalLength();
+        const startPos = pathElement.getPointAtLength(0);
+        const endPos = pathElement.getPointAtLength(length);
+
+        let startNode = this.getNodeByPos(startPos.x, startPos.y);
+
+        if(undefined === startNode){
+            startNode = this.createNode(startPos.x, startPos.y);
+        }
+
+
+        let endNode = this.getNodeByPos(endPos.x, endPos.y);
+
+        if(undefined === endNode){
+            endNode = this.createNode(endPos.x, endPos.y);
+        }
+
+        const edge:Edge = {
+            start: startNode.id,
+            end: endNode.id,
+            length
+        };
+
+        this.edges.push(edge);
+
+    }
+
+    public getNodes():Node[]{
+        return this.nodes;
+    }
+
+    private reduceDecimals(num:number){
+
+        return Math.floor(num * 10)
+
+    }
+
+    private getNodeByPos(x:number, y:number):Node{
+
+        const roundedX = this.reduceDecimals(x);
+        const roundedY = this.reduceDecimals(y);
+
+        const id = this.nodesMapping[`${roundedX}-${roundedY}`];
+
+        const node = this.getNodeById(id);
+
+        return node;
+
+    }
+
+    private getNodeById(id:number):Node{
+
+        if (undefined === id){
+            return undefined;
+        }
+
+        const node = this.nodes.find((node)=>{
+            return (node.id === id);
+        });
+
+        return node;
+
+    }
+
+
+    private createNode(x:number, y:number):Node{
+
+        const node:Node = {
+            id: this.getNextNodeId(),
+            x,
+            y
+        };
+
+        this.nodes.push(node);
+
+        const roundedX = this.reduceDecimals(x);
+        const roundedY = this.reduceDecimals(y);
+
+        this.nodesMapping[`${roundedX}-${roundedY}`] = node.id;
+
+        return node;
+
+    }
+
+    private getNextNodeId():number{
+
+        const id = this.nextNodeId;
+
+        this.nextNodeId += 1;
+
+        return id;
+
+    }
+
+    private getEdgesOnNode(id:number):Edge[]{
+
+    }
+
+
+    public static get(segments:d3.Selection<d3.BaseType, unknown, any, unknown>){
+
+        return new Graph(segments);
+
+    }
+
+    public getRoute(start:number, end:number){
+        console.log(start, end);
+
+
+
+
+    }
+
+}
---
+++ frontend/components/Map/Directions/__test__/Graph.test.ts
@@ -0,0 +1,82 @@
+import {getStrapiUrl} from "../../../strapi-api/utils/get-strapi-url";
+
+const fetch = require('node-fetch');
+
+describe('dump current database', () => {
+
+    test('dump animals', async () => {
+
+        const requestUrl = getStrapiUrl('/animals?_publicationState=preview&_limit=-1');
+
+        await fetch(requestUrl)
+        .then(
+            (res:any) => {
+                return res.json();
+            })
+        .then(async (json:any) => {
+
+            expect(json.length).toBe(252);
+            expect(JSON.stringify(json, null, 4)).toMatchSnapshot();
+
+        });
+
+    });
+
+    test('dump facilities', async () => {
+
+        const requestUrl = getStrapiUrl('/facilities?_publicationState=preview&_limit=-1');
+
+        await fetch(requestUrl)
+            .then(
+                (res:any) => {
+                    return res.json();
+                })
+            .then(async (json:any) => {
+
+                expect(json.length).toBe(47);
+                expect(JSON.stringify(json, null, 4)).toMatchSnapshot();
+
+            });
+
+    });
+
+    test('dump individual-animals', async () => {
+
+        const requestUrl = getStrapiUrl('/individual-animals?_publicationState=preview&_limit=-1');
+
+        await fetch(requestUrl)
+            .then(
+                (res:any) => {
+                    return res.json();
+                })
+            .then(async (json:any) => {
+
+                expect(json.length).toBe(1);
+                expect(JSON.stringify(json, null, 4)).toMatchSnapshot();
+
+            });
+
+    });
+
+    test('dump map-elements', async () => {
+
+        const requestUrl = getStrapiUrl('/map-elements?_publicationState=preview&_limit=-1');
+
+        await fetch(requestUrl)
+            .then(
+                (res:any) => {
+                    return res.json();
+                })
+            .then(async (json:any) => {
+
+                expect(json.length).toBe(43);
+                expect(JSON.stringify(json, null, 4)).toMatchSnapshot();
+
+            });
+
+    });
+
+});
+
+
+

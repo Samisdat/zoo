3322e4005100bc58ac2d70cb3736e3a5766d496d
samisdat <bastian@pertz.eu>
2022-02-23 22:22:53 +0100

    Split marker images into an plural and a singular component and render without fx

---
+++ frontend/components/Map/Cartesian/Markers/MarkerImage.tsx
@@ -0,0 +1,63 @@
+import React, {useEffect, useRef} from 'react';
+import * as d3 from 'd3';
+import {getImagePath} from "../../../../helper/getImagePath";
+import {Facility} from "strapi-api/entity/facility/facility";
+
+export interface MarkerImageProps {
+    facility: Facility;
+};
+
+const getImage = (facility:Facility) => {
+
+    if(!facility){
+        return undefined;
+    }
+
+    let image:string = undefined;
+
+    if(0 !== facility.photos.length && undefined !== facility.photos[0] && facility.photos[0].thumbnail){
+        image = getImagePath(facility.photos[0].thumbnail.src);
+    }
+
+    if(undefined === image){
+
+        const animalWithImage = facility.animals.find((animal)=>{
+            return (0 < animal.photos.length);
+        });
+
+        if(undefined !== animalWithImage){
+
+            if(0 !== animalWithImage.photos.length && undefined !== animalWithImage.photos[0] && animalWithImage.photos[0].thumbnail){
+                image = getImagePath(animalWithImage.photos[0].thumbnail.src);
+            }
+
+        }
+
+    }
+
+    return image;
+
+}
+
+export const MarkerImage = ({facility}:MarkerImageProps) => {
+
+    return (
+        <defs>
+            <pattern
+                id={`icon-${facility.slug}`}
+                width={1}
+                height={1}
+                patternContentUnits={'objectBoundingBox'}
+            >
+                <image
+                    href={getImage(facility)}
+                    x={0}
+                    y={0}
+                    height={1}
+                    width={1}
+                    preserveAspectRatio={'xMinYMin slice'}
+                />
+            </pattern>
+        </defs>
+    );
+}
--- frontend/components/Map/Cartesian/Markers/MarkerImages.tsx
+++ frontend/components/Map/Cartesian/Markers/MarkerImages.tsx
@@ -2,78 +2,32 @@ import React, {useEffect, useRef} from 'react';
 import * as d3 from 'd3';
 import {getImagePath} from "../../../../helper/getImagePath";
 import {Facility} from "strapi-api/entity/facility/facility";
+import {MarkerImage} from "./MarkerImage";
 
-export interface MarkerImagesProperties{
+export interface MarkerImagesProps {
     facilities: Facility[];
 };
 
-const getImage = (facility:Facility) => {
-
-    if(!facility){
-        return undefined;
-    }
-
-    let image:string = undefined;
-
-    if(0 !== facility.photos.length && undefined !== facility.photos[0] && facility.photos[0].thumbnail){
-        image = getImagePath(facility.photos[0].thumbnail.src);
-    }
-
-    if(undefined === image){
-
-        const animalWithImage = facility.animals.find((animal)=>{
-            return (0 < animal.photos.length);
-        });
-
-        if(undefined !== animalWithImage){
-
-            if(0 !== animalWithImage.photos.length && undefined !== animalWithImage.photos[0] && animalWithImage.photos[0].thumbnail){
-                image = getImagePath(animalWithImage.photos[0].thumbnail.src);
-            }
-
-        }
-
-    }
-
-    return image;
-
-}
-
-export const MarkerImages = ({facilities}:MarkerImagesProperties) => {
-
-    const markerImagesGroup = useRef(null);
+export const MarkerImages = ({facilities}:MarkerImagesProps) => {
 
     const facilitiesWithImages = facilities.filter((facility)=>{
         return (0 !== facility.photos.length)
     });
 
-    useEffect(() => {
-
-        var groupSelection = d3.select(markerImagesGroup.current);
-
-        groupSelection.selectAll('defs')
-            .data(facilitiesWithImages)
-            .join("defs")
-            .append('pattern')
-            .attr('id', function(d) { return (`icon-${d.slug}`);}) // just create a unique id (id comes from the json)
-            .attr('width', 1)
-            .attr('height', 1)
-            .attr('patternContentUnits', 'objectBoundingBox')
-            .append("image")
-            .attr("xlink:href", (facility:Facility)=>{
-                return getImage(facility);
-            })
-            .attr("x", 0)
-            .attr("y", 0)
-            .attr("height", 1)
-            .attr("width", 1)
-            .attr("preserveAspectRatio", "xMinYMin slice");
-
-
-    },[]);
-
     return (
-        <g ref={markerImagesGroup} />
+        <g>
+            {
+                facilitiesWithImages.map((facility, i) => {
+                    return (
+                        <MarkerImage
+                            key={i}
+                            facility={facility}
+                        />
+                    );
+
+                })
+            }
+        </g>
     );
 
 }

9d05d0c0c7974afe2fc45f262d65c7691280562c
samisdat <bastian@pertz.eu>
2022-07-28 17:57:24 +0200

    Youtube player

---
+++ frontend/components/Contents/Caption.tsx
@@ -0,0 +1,6 @@
+import {styled} from "@mui/material/styles";
+
+export const CaptionStyled = styled('div')(({ theme }) => ({
+    fontSize:'14px',
+    marginBottom: theme.spacing(2),
+}));
--- frontend/components/Contents/Youtube/Youtube.tsx
+++ frontend/components/Contents/Youtube/Youtube.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {ContentPart} from "../Contents";
-import {WrapImage} from "../Image/Image";
+import ReactPlayer from "react-player";
+import {styled} from "@mui/material/styles";
+import {CaptionStyled} from "../Caption";
 
 export interface YoutubeProps extends ContentPart{
     type: 'youtube',
@@ -8,14 +10,60 @@ export interface YoutubeProps extends ContentPart{
     caption: string;
 }
 
+const AspectRatioStyled = styled('div')(({ theme }) => ({
+    position: 'relative',
+    overflow: 'hidden',
+    height: 0,
+    width: '100%',
+    paddingTop: '56.25%',
+    marginBottom: theme.spacing(1),
+}));
+
+const ReactPlayerStyled = styled(ReactPlayer)(({ theme }) => ({
+    position: 'absolute',
+    top: 0,
+    left:0,
+    width: '100%',
+    height: '100%',
+    marginBottom: theme.spacing(3),
+}));
+
 export const Youtube = ({youtubeUrl, caption}:YoutubeProps) => {
 
+    const [isFrontend, setIsFrontend] = useState(false);
+
+    useEffect(()=>{
+        setIsFrontend(true);
+    });
+
+    if(!isFrontend){
+
+        return (
+            <>
+                <AspectRatioStyled />
+                <CaptionStyled>{caption}</CaptionStyled>
+            </>
+        );
+    }
+
     return (
-        <WrapImage
-            align={'fullsize'}
-        >
-            {youtubeUrl}
-            {caption}
-        </WrapImage>
+        <>
+            <AspectRatioStyled>
+                <ReactPlayerStyled
+                    width={'100%'}
+                    height={'100%'}
+                    url={youtubeUrl}
+                    config={{
+                        youtube: {
+                            playerVars: { showinfo: 1 }
+                        }
+                    }}
+                />
+
+            </AspectRatioStyled>
+            <CaptionStyled>{caption}</CaptionStyled>
+        </>
+
     );
+
 }

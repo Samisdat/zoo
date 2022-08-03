fa22c3bfa32551fafb90b949b66164dea8299eae
samisdat <bastian@pertz.eu>
2022-08-03 22:23:58 +0200

    style link within text

--- frontend/components/Contents/Text/Text.tsx
+++ frontend/components/Contents/Text/Text.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import {ContentPart} from "../Contents";
 import Page from "../../Page/Page";
+import {styled} from "@mui/material/styles";
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const ReactMarkdown = require('react-markdown')
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const gfm = require('remark-gfm')
+
+const StyledText = styled('div')(({ theme }) => ({
+
+    '& a': {
+        fontWeight: 'bold',
+        textDecoration: 'none',
+        color: theme.palette.primary.main,
+        ':hover':{
+            textDecoration: 'underline',
+        }
+    },
+}));
 
 export interface TextProps extends ContentPart{
     type: 'text',
@@ -14,13 +27,11 @@ export interface TextProps extends ContentPart{
 
 export const Text = ({text}:TextProps) => {
 
-    console.log(text)
-
     return (
-
-        <ReactMarkdown plugins={[gfm]}>
-            {text}
-        </ReactMarkdown>
-
+        <StyledText>
+            <ReactMarkdown plugins={[gfm]}>
+                {text}
+            </ReactMarkdown>
+        </StyledText>
     );
 }

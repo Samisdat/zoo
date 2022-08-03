e8e794e56c1058da20c9ed43bda0f21cab7f7599
samisdat <bastian@pertz.eu>
2022-08-03 22:25:49 +0200

    Loop over images and put them into grid

--- frontend/components/Contents/Images/Images.tsx
+++ frontend/components/Contents/Images/Images.tsx
@@ -2,23 +2,38 @@ import React from 'react';
 import {ContentPart} from "../Contents";
 import {PhotoJson} from "../../../data/graphql/photo/photo-json";
 import {Image} from "../Image/Image";
+import Grid from "@mui/material/Grid";
 
 export interface ImagesProps extends ContentPart{
     type: 'images',
     images: PhotoJson[];
 }
 
+
 export const Images = ({images}:ImagesProps) => {
 
     return (
-        <>
-            {images.map((image:PhotoJson, index) => (
-                <Image
-                    type={'image'}
-                    image={image}
-                    align={'none'}
-                />
-            ))}
-        </>
+            <Grid
+                container
+                spacing={2}
+            >
+                {images.map((image:PhotoJson, index) => (
+                    <Grid
+                        item
+                        xs={12}
+                        sm={6}
+                        key={index}
+                    >
+                        <div>
+                            <Image
+                                type={'image'}
+                                image={image}
+                                align={'none'}
+                            />
+                        </div>
+                    </Grid>
+
+                ))}
+            </Grid>
     );
 }
